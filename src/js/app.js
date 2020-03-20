3d008a88b0f9945cc0b82ecb6c09a83603a0044a
FireRipper <shabelnyk.ilya@gmail.com>
2020-03-20 17:50:28 +0200

    chore!: working app. Add to app.js - new events listener, add handlers - addTicketToFavoriteList, deleteTicketFromFavorite and add process exception when submit form with empty fields.

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import locations from './store/locations'
 import formUI from './views/form'
 import currencyUI from './views/currency'
 import ticketsUI from './views/tickets'
+import favorite from './store/favorite'
+import favoriteTicketsUI from './views/favorite-tickets'
 
 import '../css/style.css'
 import './plugins'
@@ -10,14 +12,37 @@ document.addEventListener('DOMContentLoaded', () => {
     initApp()
 
     const form = formUI.form
+    const ticket = ticketsUI.ticketsSections
+    const favoriteTicket = favoriteTicketsUI.favoriteSection
+
     //Events
     form.addEventListener('submit', e => {
         e.preventDefault()
 
+        if (!formUI.originValue || !formUI.destinationValue || !formUI.departDateValue) {
+            M.toast({
+                html: 'Please, fill in all the fields',
+                classes: 'rounded light-blue darken-1'
+            })
+            return
+        }
+
         onFormSubmit()
     })
 
+    ticket.addEventListener('click', addTicketToFavoriteList)
+    favoriteTicket.addEventListener('click', deleteTicketFromFavorite)
+
     //Handlers
+    function addTicketToFavoriteList({ target }) {
+        favorite.getTicket(target)
+        favoriteTicketsUI.renderFavoriteTicketsList(favorite.favoriteTicketsList)
+    }
+
+    function deleteTicketFromFavorite({ target }) {
+        favorite.deleteTicketFromFavoriteList(target)
+    }
+
     async function initApp() {
         await locations.init()
         formUI.setAutocompleteData(locations.shortCitiesList)
@@ -40,5 +65,6 @@ document.addEventListener('DOMContentLoaded', () => {
         })
 
         ticketsUI.renderTickets(locations.lastSearch)
+        locations.convertTicketsToObject(locations.lastSearch)
     }
 })
